b55a7031bfc546981e9de13dfd3e71ed8259d132
JSnyder593 <jaeger_snyder@hotmail.com>
2022-11-23 13:55:19 -0800

    forgot to save Thought model changes

--- Models/Thought.js
+++ Models/Thought.js
@@ -50,8 +50,12 @@ const thoughtSchema = new Schema(
             getters: true,
         },
         id: false,
-    },
+    }
 );
+
+thoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+});
 
 const Thought = model('thought', thoughtSchema);
 
