709e9a647fed2717735146c616cc5f96e3e06ef6
JSnyder593 <jaeger_snyder@hotmail.com>
2022-11-23 14:14:10 -0800

    user and thought routes pseudocoding

---
+++ Routes/API/thoughtRoutes.js
@@ -0,0 +1,5 @@
+//express and controllers
+
+//thought functions (all thoughts), (one thought), (create thought), (update thought), (delete thought), (add a reaction), (remove a reaction)
+
+//routes (api/thoughts), (api/thoughts/:id), (api/thoughts/:thoughtid/reactions), (api/thoughts/:thoughtid/reactions/:reactionid) 
---
+++ Routes/API/userRoutes.js
@@ -0,0 +1,5 @@
+//express and controllers
+
+//user functions (get all), (get one), (create user), (update user), (delete user), (add friend), (delete friend)
+
+//routes (api/users), (api/users/:id), (api/users/friends/:friendID) 
